c61baa3b94f8c3ba59d465c93d39c61095b01e6e
nricom <nricom@eafit.edu.co>
2025-10-22 07:12:38 -0500

    First commit

---
+++ Parcial02.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/factorial/<int:num>')
+def calcular_factorial(num):
+    
+    # Calcular factorial
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+
+    # Determinar si el factorial es par o impar
+    etiqueta = "par" if factorial % 2 == 0 else "impar"
+
+    # Construir respuesta JSON
+    respuesta = {
+        "numero_recibido": num,
+        "factorial": factorial,
+        "etiqueta": etiqueta
+    }
+
+    return jsonify(respuesta), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
